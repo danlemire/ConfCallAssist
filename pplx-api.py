35de6a920fa49ca46ce66a1f555d3766c3e0df46
Daniel Lemire <github@danlemire.com>
2024-02-10 21:09:33 -0600

    working combo

--- pplx-api.py
+++ pplx-api.py
@@ -1,19 +1,23 @@
 from openai import OpenAI
+import os
 
-YOUR_API_KEY = "INSERT API KEY HERE"
-
+YOUR_API_KEY = os.environ.get("PPLX_API_KEY")
+#YOUR_API_KEY = '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'
 messages = [
     {
         "role": "system",
         "content": (
-            "You are an artificial intelligence assistant and you need to "
-            "engage in a helpful, detailed, polite conversation with a user."
+            "You are an artificial intelligence assistant and you will to "
+            "take this conversation, understand the important points "
+            "and provide helpful, detailed, information to help the user "
+            "to continue the conversation by pointing out the most relevant "
+            "related information."
         ),
     },
     {
         "role": "user",
         "content": (
-            "How many stars are in the universe?"
+            "what is RAG (retrieval augmented generation) in the context of Generative AI?"
         ),
     },
 ]
@@ -28,10 +32,12 @@ response = client.chat.completions.create(
 print(response)
 
 # chat completion with streaming
-response_stream = client.chat.completions.create(
+stream = client.chat.completions.create(
     model="mistral-7b-instruct",
     messages=messages,
     stream=True,
 )
-for response in response_stream:
-    print(response)
+
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="")
